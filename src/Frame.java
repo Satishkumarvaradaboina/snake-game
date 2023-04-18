4a40ba31c597af6d4cba49924e94b30ce0982f56
satishkumar <satishkumar123@gmail.com>
2023-04-18 21:01:39 +0530

    first commit

---
+++ src/Frame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+import  javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    Frame()
+    {
+        this.setTitle("SnakeGame");
+        this.add(new Panel());
+        this.setVisible(true);
+        this.setResizable(false);
+        this.pack();
+
+
+}}
